365426eba177dcf93c0d684780eb5ba05c8664ba
Befend <18814382464@163.com>
2023-04-18 08:37:36 +0800

    feat(*): add global components.

---
+++ src/components/SwitchDark/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts" name="SwitchDark">
+import { computed } from "vue"
+import { GlobalStore } from "@/store"
+import { Sunny, Moon } from "@element-plus/icons-vue"
+import { useTheme } from "@/hooks/useTheme"
+
+const { switchDark } = useTheme()
+
+const globalStore = GlobalStore()
+const themeConfig = computed(() => globalStore.themeConfig)
+</script>
+
+<template>
+  <el-switch v-model="themeConfig.isDark" @change="switchDark" inline-prompt :active-icon="Sunny" :inactive-icon="Moon" />
+</template>
