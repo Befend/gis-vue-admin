4309089dda88ac51eb84a354a102ff42d825b9ac
Befend <18814382464@163.com>
2023-04-18 23:49:08 +0800

    feat(table): add table module.

---
+++ src/views/proTable/useProTable/detail.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts" name="useProTableDetail">
+import { useRoute } from "vue-router"
+const route = useRoute()
+</script>
+
+<template>
+  <div class="card content-box">
+    <span class="text"> 我是 ProTable 详情页，属于 ProTable 下面的子集</span>
+    <span class="text">params:{{ route.params }}</span>
+    <span class="text">query:{{ route.query }}</span>
+  </div>
+</template>
---
+++ src/views/proTable/useTreeFilter/detail.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts" name="useTreeFilter">
+import { useRoute } from "vue-router"
+const route = useRoute()
+</script>
+
+<template>
+  <div class="card content-box">
+    <span class="text"> 我是 TreeFilter 详情页，和 TreeFilter 是同一级路由</span>
+    <span class="text">params:{{ route.params }}</span>
+    <span class="text">query:{{ route.query }}</span>
+  </div>
+</template>
