d0db4557f715f8e410ff082a05e19234350291ab
Befend <18814382464@163.com>
2023-04-18 23:31:40 +0800

    feat(auth): add auth page.

---
+++ src/views/auth/button/index.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts" name="authButton">
+import { useAuthButtons } from "@/hooks/useAuthButtons"
+import { CirclePlus, Delete, EditPen, Download, Upload } from "@element-plus/icons-vue"
+
+const { BUTTONS } = useAuthButtons()
+</script>
+
+<template>
+  <div class="card content-box">
+    <span class="text"> 按钮权限</span>
+    <el-alert
+      class="mb20"
+      :title="`当前用户按钮权限：${JSON.stringify(Object.keys(BUTTONS))}`"
+      type="success"
+      :closable="false"
+    />
+    <el-divider content-position="left">使用 Hooks 方式绑定权限</el-divider>
+    <el-row class="mb20">
+      <el-button type="primary" :icon="CirclePlus" v-if="BUTTONS.add">新增</el-button>
+      <el-button type="warning" :icon="EditPen" v-if="BUTTONS.edit">编辑</el-button>
+      <el-button type="danger" plain :icon="Delete" v-if="BUTTONS.delete">删除</el-button>
+      <el-button type="info" plain :icon="Upload" v-if="BUTTONS.import">导入数据</el-button>
+      <el-button type="info" plain :icon="Download" v-if="BUTTONS.export">导出数据</el-button>
+    </el-row>
+    <el-divider content-position="left">使用 v-auth 指令绑定单个权限</el-divider>
+    <el-row class="mb20">
+      <el-button type="primary" :icon="CirclePlus" v-auth="'add'">新增</el-button>
+      <el-button type="warning" :icon="EditPen" v-auth="'edit'">编辑</el-button>
+      <el-button type="danger" plain :icon="Delete" v-auth="'delete'">删除</el-button>
+      <el-button type="info" plain :icon="Upload" v-auth="'import'">导入数据</el-button>
+      <el-button type="info" plain :icon="Download" v-auth="'export'">导出数据</el-button>
+    </el-row>
+    <el-divider content-position="left">使用 v-auth 指令绑定多个权限</el-divider>
+    <el-row>
+      <el-button type="primary" :icon="CirclePlus" v-auth="['add', 'edit', 'delete', 'import', 'export']">新增</el-button>
+      <el-button type="warning" :icon="EditPen" v-auth="['add', 'edit', 'delete', 'import', 'export']">编辑</el-button>
+      <el-button type="danger" plain :icon="Delete" v-auth="['add', 'edit', 'delete', 'import', 'export']">删除</el-button>
+      <el-button type="info" plain :icon="Upload" v-auth="['add', 'edit', 'delete', 'import', 'export']">导入数据</el-button>
+      <el-button type="info" plain :icon="Download" v-auth="['add', 'edit', 'delete', 'import', 'export']">导出数据</el-button>
+    </el-row>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.content-box {
+  align-items: flex-start;
+  span {
+    width: 100%;
+    text-align: center;
+  }
+}
+</style>
---
+++ src/views/auth/menu/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts" name="authMenu">
+import { useRouter } from "vue-router"
+import { LOGIN_URL } from "@/config/config"
+import { GlobalStore } from "@/store"
+import { Promotion } from "@element-plus/icons-vue"
+
+const router = useRouter()
+const globalStore = GlobalStore()
+
+const handleToLogin = () => {
+  globalStore.setToken("")
+  router.push(LOGIN_URL)
+}
+</script>
+
+<template>
+  <div class="card content-box">
+    <span class="text"> 菜单权限</span>
+    <el-alert
+      :title="'目前菜单权限使用动态路由实现，模拟后台根据不同用户角色返回对应路由，注意观察左侧菜单变化（admin 账号可查看所有菜单、user 账号只可查看部分菜单）'"
+      type="success"
+      :closable="false"
+    />
+    <el-button class="mt20" type="primary" :icon="Promotion" @click="handleToLogin">登录其他账号</el-button>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
